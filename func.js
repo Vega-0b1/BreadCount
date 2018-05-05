ee414d8d693a41d0c903a24cca3860263a8dba66
Juan Carlos Vega <jcvega0b1@gmail.com>
2018-05-05 14:50:31 -0700

    change some css and continued refactoring code (looking clean)

--- func.js
+++ func.js
@@ -21,7 +21,7 @@ function countBread(){
    var saladOpen = parseFloat(document.getElementById("saladOpen").value);
    var wrapFront1= parseFloat(document.getElementById("wrapFront1").value);
    var wrapFront2= parseFloat(document.getElementById("wrapFront2").value);
-   var retarderBread = parseFloat(document.getElementById("retarderTotal").value);
+   var retarderBread = parseFloat(document.getElementById("retarderBread").value);
 
    var saladTotal = (saladBag*56) + saladOpen;
    var freezerWrap = (wrapBox*72) + (wrapOpen*8);
@@ -32,8 +32,9 @@ function countBread(){
    var breadTotal = freezerBread + (retarderBread * 10) + breadFront1 + breadFront2;
    var flatTotal = freezerFlat + flatFront1 + flatFront2;
    var wrapTotal = freezerCross + freezerWrap + wrapFront1 + wrapFront2 + crossFront;
-   var dataFields = {breadBox, breadOpen, flatBox, flatOpen, crossBox, crossOpen, wrapBox, wrapOpen,
-                     breadFront1, breadFront2, flatFront1, flatFront2, crossFront, saladBag, saladOpen, wrapFront1, wrapFront2, retarderBread}
+
+   var dataFields = {breadBox, breadOpen, flatBox, flatOpen, crossBox, crossOpen, retarderBread, wrapBox, wrapOpen,
+                     breadFront1, flatFront1, wrapFront1, crossFront, breadFront2, flatFront2, wrapFront2, saladBag, saladOpen};
 
    localStorage.setItem("data", JSON.stringify(dataFields));
    updateTotal(breadTotal, flatTotal, wrapTotal, saladTotal);
@@ -50,28 +51,10 @@ function populateFields(){
    var data = localStorage.getItem("data");
    data = JSON.parse(data);
 
-   document.getElementById("breadBox").value = data.breadBox;
-   document.getElementById("breadOpen").value = data.breadOpen;
-   document.getElementById("flatBox").value = data.flatBox;
-   document.getElementById("flatOpen").value = data.flatOpen;
-   document.getElementById("crossBox").value = data.crossBox;
-   document.getElementById("crossOpen").value = data.crossOpen;
-   document.getElementById("wrapBox").value = data.wrapBox;
-   document.getElementById("wrapOpen").value = data.wrapOpen;
-
-   document.getElementById("breadFront1").value = data.breadFront1;
-   document.getElementById("breadFront2").value = data.breadFront2;
-   document.getElementById("flatFront1").value = data.flatFront1;
-   document.getElementById("flatFront2").value = data.flatFront2;
-   document.getElementById("wrapFront1").value = data.wrapFront1;
-   document.getElementById("wrapFront2").value = data.wrapFront2;
-   document.getElementById("crossFront").value = data.crossFront;
-
-   document.getElementById("saladBag").value = data.saladBag;
-   document.getElementById("saladOpen").value = data.saladOpen;
-
-   document.getElementById("retarderTotal").value = data.retarderBread;
-
+   for(var i = 0; i <= 17; i++){
+     var id = Object.keys(data)[i];
+     document.getElementById(id).value = data[id];
+  }
  }
 
 function addEvents(){
